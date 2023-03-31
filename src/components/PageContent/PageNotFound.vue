f94750166c94d6f95eca7a49d681c591809af199
Yatharth Verma <yatharthverma.che18@iitbhu.ac.in>
2023-03-31 14:29:53 +0900

    Page not found modified

--- src/components/PageContent/PageNotFound.vue
+++ src/components/PageContent/PageNotFound.vue
@@ -1,10 +1,26 @@
  
 <template>
-    <div>
-     The requested Page does not exist
+    <div class="card-placing">
+
+        <div class="card" style="width: 18rem;">
+     <div class="card-body">
+       要求されたページが存在しない
      </div>
+   </div>
+    </div>
   </template>
 
 <style scoped>
+.card-placing {
+    display: flex;
+    flex-direction: row;
+    justify-content: center;
+}
+ .card {
+    background-color:rgb(224, 112, 112);
+ }
 
+ .card-body {
+    align-self: center;
+ }
 </style>
